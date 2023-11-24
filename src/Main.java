632a971fef145346f1be2cfdee04e70cd0dd03b3
POO <alan.rios.hd@gmail.com>
2023-11-24 11:35:44 -0500

    alan.rios.hd@gmail.com

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public int cedula;
+    public String nombre;
+    public String apellido;
+
+    public Persona(int cedula, String nombre, String apellido){
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
