c167c9d5852c82d7a739275c071eb76d77ad8608
POO <alan.rios.hd@gmail.com>
2023-11-24 11:49:33 -0500

    git@github.com:AlanRiosM/Estudiante.git

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,4 @@
 public class Main {
-    public int cedula;
-    public String nombre;
-    public String apellido;
-
-    public Persona(int cedula, String nombre, String apellido){
-        this.cedula = cedula;
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
